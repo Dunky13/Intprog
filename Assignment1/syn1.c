36c5398316e46f7300d6e0cf323384e97f0b5c00
Dunky13 <mwent@live.nl>
2015-09-15 14:50:40 +0200

    syn1 & makefile

--- Assignment1/syn1.c
+++ Assignment1/syn1.c
@@ -11,12 +11,29 @@ void display(char *str) {
 }
 int main() {
     int i;
+	
+	struct sembuf up 	= {0,  1, 0};
+	struct sembuf down	= {0, -1, 0};
+	int semaphore = semget(IPC_PRIVATE, 1, 0600);
+	
+	
     if (fork()) {
-        for (i=0;i<10;i++) display("Hello world\n");
+        for (i=0;i<10;i++){ 
+			if(semop(semaphore,&down, 1) == 0){ /* SEMOP Was succesful*/
+				display("Hello world\n");
+				semop(semaphore,&up, 1);
+			}
+		}
         wait(NULL);
+		semctl(semaphore, 0, IPC_RMID);
     }
     else {
-        for (i=0;i<10;i++) display("Bonjour monde\n");
+        for (i=0;i<10;i++){
+			if(semop(semaphore,&down, 1) == 0){ /* SEMOP Was succesful*/
+				display("Bonjour monde\n");
+				semop(semaphore,&up, 1);
+			}
+		} 
     }
     return 0;
 }
