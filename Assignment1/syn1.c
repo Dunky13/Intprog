32b3881f363fbe1a485f881daec33391aab39db8
Dunky13 <mwent@live.nl>
2015-09-15 14:53:51 +0200

    test

--- Assignment1/syn1.c
+++ Assignment1/syn1.c
@@ -2,41 +2,46 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-
 #include <sys/ipc.h>
 #include <sys/sem.h>
+
 void display(char *str) {
-    char *tmp;
-    for (tmp=str;*tmp;tmp++) {
-        write(1,tmp,1);
-        usleep(100);
-    }
+  char *tmp;
+  for (tmp=str;*tmp;tmp++) {
+    write(1,tmp,1);
+    usleep(100);
+  }
 }
+
 int main() {
-    int i;
-	
-	struct sembuf up 	= {0,  1, 0};
-	struct sembuf down	= {0, -1, 0};
-	int semaphore = semget(IPC_PRIVATE, 1, 0600);
-	
-	
-    if (fork()) {
-        for (i=0;i<10;i++){ 
-			if(semop(semaphore,&down, 1) == 0){ /* SEMOP Was succesful*/
-				display("Hello world\n");
-				semop(semaphore,&up, 1);
-			}
-		}
-        wait(NULL);
-		semctl(semaphore, 0, IPC_RMID);
+  struct sembuf up = {0,1,0};
+  struct sembuf down = {0,-1,0};
+  int i, my_sem;
+
+  my_sem = semget(IPC_PRIVATE, 1, 0600); /*create semaphore*/
+  if(my_sem<0){
+    perror("Error getting semaphore:");
+  }
+  
+  if(semop(my_sem, &up, 1)){perror("error changing semaphore:");}; /*set to 1*/
+
+  if (fork()) {
+    for (i=0;i<10;i++) {
+      if(semop(my_sem, &down, 1)){perror("error changing semaphore:");}; 
+      display("Hello world\n");
+      if(semop(my_sem, &up, 1)){perror("error changing semaphore:");}; 
     }
-    else {
-        for (i=0;i<10;i++){
-			if(semop(semaphore,&down, 1) == 0){ /* SEMOP Was succesful*/
-				display("Bonjour monde\n");
-				semop(semaphore,&up, 1);
-			}
-		} 
+    wait(NULL);
+    if(semctl(my_sem,0,IPC_RMID)){perror("error destroying semaphore:");}  /*destroy the semaphore*/
+  }
+  else {
+    for (i=0;i<10;i++) {
+      if(semop(my_sem, &down, 1)){perror("error changing semaphore:");};
+      display("Bonjour monde\n");
+      if(semop(my_sem, &up, 1)){perror("error changing semaphore:");}; 
     }
-    return 0;
+  }
+
+
+  return 0;
 }
