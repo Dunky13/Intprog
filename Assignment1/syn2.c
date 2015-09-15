b455d1dc913b807176775e663db10daa256caaf4
Dunky13 <mwent@live.nl>
2015-09-15 15:25:26 +0200

    test2

--- Assignment1/syn2.c
+++ Assignment1/syn2.c
@@ -36,8 +36,11 @@ int main() {
 				perror("Locking semaphore failed");
 		}
         wait(NULL);
-		if(semctl(semaphore, 1, IPC_RMID) < 0)
+		if(semctl(semaphore, 0, IPC_RMID) < 0)
 			perror("Removing parent semaphore");
+		
+		if(semctl(semaphore, 1, IPC_RMID) < 0)
+			perror("Removing child semaphore");
     }
     else 
 	{
