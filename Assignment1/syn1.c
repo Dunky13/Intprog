377f558e3d1a41635acd76192c2b9822e9829419
Dunky13 <mwent@live.nl>
2015-09-15 15:02:03 +0200

    print error messages

--- Assignment1/syn1.c
+++ Assignment1/syn1.c
@@ -19,23 +19,35 @@ int main() {
 	struct sembuf down	= {0, -1, 0};
 	int semaphore = semget(IPC_PRIVATE, 1, 0600);
 	
-	semop(semaphore,&up, 1);
-    if (fork()) {
-        for (i=0;i<10;i++){ 
-			if(semop(semaphore,&down, 1) >= 0){ /* SEMOP Was succesful*/
+	if(semaphore < 0) perror("Could not initialize semaphore");
+	
+	if(semop(semaphore,&up, 1) < 0) perror("Locking initial semaphore failed");
+    if (fork()) 
+	{
+        for (i=0;i<10;i++)
+		{ 
+			if(semop(semaphore,&down, 1) >= 0)
+			{ /* SEMOP Was succesful*/
 				display("Hello world\n");
-				semop(semaphore,&up, 1);
-			}
+				if(semop(semaphore,&up, 1) < 0)
+					perror("Unlocking semaphore failed");
+			} else 
+				perror("Locking semaphore failed");
 		}
         wait(NULL);
 		semctl(semaphore, 0, IPC_RMID);
     }
-    else {
-        for (i=0;i<10;i++){
-			if(semop(semaphore,&down, 1) >= 0){ /* SEMOP Was succesful*/
+    else
+		{
+        for (i=0;i<10;i++)
+		{
+			if(semop(semaphore,&down, 1) >= 0)
+			{ /* SEMOP Was succesful*/
 				display("Bonjour monde\n");
-				semop(semaphore,&up, 1);
-			}
+				if(semop(semaphore,&up, 1) < 0)
+					perror("Unlocking semaphore failed");
+			} else 
+				perror("Locking semaphore failed");
 		} 
     }
     return 0;
