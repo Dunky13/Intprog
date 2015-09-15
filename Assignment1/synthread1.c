88e3313eddceca6cdc931aa233081cbb97ec578c
Dunky13 <mwent@live.nl>
2015-09-15 14:20:33 +0200

    create basic assignment 2

---
+++ Assignment1/synthread1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+void display(char *str) {
+    char *tmp;
+    for (tmp=str;*tmp;tmp++) {
+        write(1,tmp,1);
+        usleep(100);
+    }
+}
+int main() {
+    int i;
+    if (fork()) {
+        for (i=0;i<10;i++) display("Hello worldn");
+        wait(NULL);
+    }
+    else {
+        for (i=0;i<10;i++) display("Bonjour monden");
+    }
+    return 0;
+}
