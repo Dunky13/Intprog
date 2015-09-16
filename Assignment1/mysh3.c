e7bbc1d16aa2c6f1d1e391c74c81709b58162b97
Ferry <ferryavis@quicknet.nl>
2015-09-16 14:59:03 +0200

    eerste werkende versie

--- Assignment1/mysh3.c
+++ Assignment1/mysh3.c
@@ -12,9 +12,8 @@
 #include <string.h>
 
 #define BUF_SIZE 	100
-#define OUTPUT_BUF_SIZE 10000
 
-void execute(char **commands, int n_commands, int change_stdin);
+void execute(int *fd, char **commands, int index_command);
 
 void *trim(char *in, char *out){
 	char *end;
@@ -36,7 +35,8 @@ void *trim(char *in, char *out){
 	strncpy(out, in, strlen(in)+1);
 }
 
-void str_to_array(char *str, const char *delimiter, char **array, int *n_commands){
+/* splits a sstring on the delimiter and puts each resulting element in the array */
+void str_to_array(char *str, const char *delimiter, char **array){
 	char *token;
 	int i = 0;
 
@@ -49,49 +49,40 @@ void str_to_array(char *str, const char *delimiter, char **array, int *n_command
 	}
 
 	array[i+1] = NULL;
-	*n_commands = i+1;
 }
 
-void child_code(int *pipe_child_parent, int *pipe_parent_child, char *command, int use_pipe, int redir_stdin){
+/* executed at child */
+void child_code(int *fd, char *command, int change_stdout, int change_stdin){
 	char *parameters[BUF_SIZE/2+2]; //the parameters could consist of single characters seperated by a space. Add 1 for rounding and 1 for the null character to the array 
 	const char delimiter[2] = " ";
-	int n_parameters; /* only defined to pass as it is required in the line below, is not used further*/
-	str_to_array(command, delimiter, parameters, &n_parameters);
+	str_to_array(command, delimiter, parameters);
 
-	if(redir_stdin){
-		close(0);
-		dup(pipe_child_parent[0]); /* redirect stdin to read output of previous command (pipe) */
+	if(change_stdin){
+		close(fd[1]); 	/* close writer */
+
+		if(dup2(fd[0], 0) < 0){		/* redirect stdin to read output of previous command (pipe) */
+			perror("dup2 at redirecting pipe to stdin");
+		}
+	}
+	else if(change_stdout){
+		if(dup2(fd[1], 1) < 0){		/* redirect stout to pipe */
+			perror("dup2 at redirecting stdout to pipe");
+		}
+
+		close(fd[0]);	/* close reader */
 	}
 
-	if(use_pipe){
-		close(pipe_child_parent[0]); /* close reader */
-		close(1);					/* close standard output */
-		dup(pipe_child_parent[1]); /* redirect standard output to pipe */
-	}
-
-	execvp(command, parameters);
+	execvp(parameters[0], parameters);
 
 	perror("exec"); /*will not be reached in case of a success, as exec replaces the complete program*/
 	exit(-1);
 }
 
-void parent_code(int *pipe_child_parent, char **commands, int n_commands, char output_buffer[OUTPUT_BUF_SIZE]){
+/* executed at parent */
+void parent_code(int *fd, char **commands, int index_command){
 	int status;
-
-	if(n_commands > 1){	/* use pipe*/
-		if(read(pipe_child_parent[0], output_buffer, OUTPUT_BUF_SIZE) < 0){
-			perror("pipe read");
-		}
-
-		wait(&status);
-
-		if(status < 0){
-			perror("wait");
-			exit(-1);
-		}
-
-		execute(commands++, n_commands - 1, 1);
-	}
+	pid_t pid;
+	char command[BUF_SIZE];
 
 	wait(&status);
 
@@ -99,37 +90,8 @@ void parent_code(int *pipe_child_parent, char **commands, int n_commands, char o
 		perror("wait");
 		exit(-1);
 	}
-}
 
-void execute(char **commands, int n_commands, int change_stdin){
-	pid_t pid;
-	int pipe_child_parent[2];
-	int pipe_parent_child[2];
-	char command[BUF_SIZE];
-	char output_buffer[OUTPUT_BUF_SIZE];
-
-	trim(commands[0], command);
-
-	if(strcmp("exit", command) == 0){
-		printf("exiting...\n");
-		exit(0);
-	}
-	else if(strcmp("", command) != 0){
-		int use_pipe = n_commands > 1 || change_stdin;
-
-		/*open pipe if needed*/
-		if(use_pipe && pipe(pipe_child_parent) < 0){
-			perror("pipe");
-			exit(-1);
-		}
-
-		/*if(change_stdin){
-			if(pipe(pipe_parent_child) < 0){
-				perror("pipe");
-				exit(-1);
-			}
-		}*/
-
+	if(commands[index_command+1] != NULL){
 		pid = fork();
 
 		if(pid < 0){
@@ -137,16 +99,52 @@ void execute(char **commands, int n_commands, int change_stdin){
 			exit(-1);
 		}
 		else if(pid == 0){	/*child*/
-			child_code(pipe_child_parent, pipe_parent_child, command, use_pipe, change_stdin);
+			trim(commands[1], command);
+
+			child_code(fd, command, 0, 1);
 		}
 		else{				/* parent */
-			//if(change_stdin){
-				//close(pipe_parent_child[0]); /* close reader */
-				//write(pipe_parent_child[1], output_buffer, OUTPUT_BUF_SIZE);
-				//close(pipe_parent_child[1]); /* close writer */
-			//}*/
+			close(fd[0]);
+			close(fd[1]);
 
-			parent_code(pipe_child_parent, commands, n_commands, output_buffer);
+			wait(&status);
+
+			if(status < 0){
+				perror("wait");
+				exit(-1);
+			}
+		}
+	}			
+
+	/*if(commands[index_command+1] != NULL){
+		execute(fd, commands, index_command+1);
+	}*/
+}
+
+void execute(int *fd, char **commands, int index_command){
+	pid_t pid;
+	char command[BUF_SIZE];
+	int use_pipe;
+
+	trim(commands[index_command], command);
+
+	if(strcmp("exit", command) == 0){
+		printf("exiting...\n");
+		exit(0);
+	}
+	else if(strcmp("", command) != 0){
+		pid = fork();
+
+		if(pid < 0){
+			perror("fork");
+			exit(-1);
+		}
+		else if(pid == 0){	/*child*/
+			use_pipe = commands[index_command+1] != NULL;			
+			child_code(fd, command, use_pipe, index_command > 0);
+		}
+		else{				/* parent */
+			parent_code(fd, commands, index_command);			
 		}
 	}
 }
@@ -160,13 +158,18 @@ main(int argc, char **argv){
 	char input[BUF_SIZE];
 	char *commands[BUF_SIZE/2+2]; //the commands could consist of single characters seperated by a |. Add 1 for rounding and 1 for the null character to the array 
 	const char delimiter[2] = "|";
-	int n_commands;
+	int fd[2];
 
 	printf("\nCommand: ");
 
 	while(fgets(input, sizeof(input), stdin)){
-		str_to_array(input, delimiter, commands, &n_commands); /* split entered commands by | symbol */
-		execute(commands, n_commands, 0);
+		if(pipe(fd) < 0){
+			perror("pipe");
+			exit(-1);
+		}
+
+		str_to_array(input, delimiter, commands); /* split entered commands by | symbol */
+		execute(fd, commands, 0);
 		printf("\nCommand: ");		
 	}
 
