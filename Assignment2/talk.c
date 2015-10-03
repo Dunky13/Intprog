819dfd54df3e301a984a1ac3ce0c1fb1f0e9bcbf
Marc Went <Dunky13@users.noreply.github.com>
2015-10-03 23:36:06 +0200

    Update talk.c

--- Assignment2/talk.c
+++ Assignment2/talk.c
@@ -122,7 +122,7 @@ WINDOW *create_newwin(int height, int width, int starty, int startx)
 {	WINDOW *local_win;
 
 	local_win = newwin(height, width, starty, startx);
-	wborder(local_win, '', '', '', '-', '', '', '', '');
+	wborder(local_win, ' ', ' ', ' ', '-', ' ', ' ', ' ', ' ');
 	
 	wrefresh(local_win);		/* Show that box 		*/
 
