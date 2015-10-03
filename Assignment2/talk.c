2e29bfee6f834a3a019037e1b69c1afda2a7a468
Marc Went <Dunky13@users.noreply.github.com>
2015-10-03 23:32:08 +0200

    Update talk.c

--- Assignment2/talk.c
+++ Assignment2/talk.c
@@ -144,8 +144,8 @@ void startThreads(struct ThreadVariables threadVariables){
 	
 	height = LINES / 2;
 	
-	threadVariables.writeWindow = &create_newwin(height, COLS, 0,0);
-	threadVariables.readWindow = &create_newwin(height-1,COLS,height+1,0);
+	threadVariables.writeWindow = create_newwin(height, COLS, 0,0);
+	threadVariables.readWindow = create_newwin(height-1,COLS,height+1,0);
 	
 	pthread_create(&readThread, &attr, readFrom, (void *)&threadVariables);
 	pthread_create(&writeThread, &attr, writeTo, (void *)&threadVariables);
