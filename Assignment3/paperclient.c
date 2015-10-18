e2f2dbb89d1feac73333902123f60c15f75fbabb
Marc Went <Dunky13@users.noreply.github.com>
2015-10-18 17:15:51 +0200

    Update paperclient.c

--- Assignment3/paperclient.c
+++ Assignment3/paperclient.c
@@ -143,7 +143,7 @@ int addArticle(CLIENT *cl, char* author, char* title, char* file_path)
 	in->title 		= strdup(title);
 	in->paper 		= readFile(file_path);
 
-	printf("Article: %s - %s\n", in->author, in->title);
+	printf("Article: %s - %s, %d\n", in->author, in->title, strlen(in->paper));
 
 	out = add_paper_1(in, cl);
 
