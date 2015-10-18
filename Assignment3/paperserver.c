2c5a6fdd2d888083cc01c414e4f7419b45a715c4
Marc Went <Dunky13@users.noreply.github.com>
2015-10-18 22:08:25 +0200

    Update paperserver.c

--- Assignment3/paperserver.c
+++ Assignment3/paperserver.c
@@ -180,8 +180,8 @@ struct paper_information *fetch_paper_1_svc(int_in *in, struct svc_req *req)
 
 	paper->paper_out_len = curr->paper_info->paper.paper_len;
 
-	//printf("It sould be copied: %u", paperInfo->paper_out_len);
-	out->paper.paper_val = paper->paper_out_val;
+	memcpy(&(out->paper.paper_val), paper->paper_out_val, paper->paper_out_len);
+	//out->paper.paper_val = paper->paper_out_val;
 	out->paper.paper_len = paper->paper_out_len;
 	return out;
 }
