bcd485bd8662c2496366c355ed6dfad62de10020
Marc Went <Dunky13@users.noreply.github.com>
2015-10-18 18:47:12 +0200

    Update paperserver.c

--- Assignment3/paperserver.c
+++ Assignment3/paperserver.c
@@ -25,12 +25,12 @@ void freePreviousInfoOut(struct paper_information* out)
 	{
 		return;
 	}
-	free(out->author);
 	free(out->title);
-	if(&(out->paper) != NULL)
+	if((out->paper) != NULL)
 	{
 		free(out->paper.paper_val);
 	}
+	free(out->author);
 	free(out);
 }
 void freePreviousListOut(struct paper_list_out* out)
@@ -203,8 +203,8 @@ paper_information *info_paper_1_svc(int_in *in, struct svc_req *req)
 
 	if(!hasPapers())
 	{
-		out->author = "x";
-		out->title = "x";
+		out->author = "";
+		out->title = "";
 		return out;
 	}
 	curr = closer(id, head, tail); //Not necessarily best option:
