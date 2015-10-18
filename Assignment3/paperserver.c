a14f33de9a93b71059f0fd115101fa3ef11a8be1
Marc Went <Dunky13@users.noreply.github.com>
2015-10-18 21:22:51 +0200

    Update paperserver.c

--- Assignment3/paperserver.c
+++ Assignment3/paperserver.c
@@ -172,12 +172,13 @@ struct paper_information *fetch_paper_1_svc(int_in *in, struct svc_req *req)
 	}
 
 	printf("Found the paper: %ld\n", curr->paper_info->paper.paper_len);
+	out->paper.paper_len = 0;
 	out->paper.paper_val = malloc(curr->paper_info->paper.paper_len * sizeof(char));
 	memcpy(&(out->paper.paper_val),
 		&(curr->paper_info->paper.paper_val),
 		curr->paper_info->paper.paper_len
 	);
-	(&out->paper)->paper_len = curr->paper_info->paper.paper_len;
+	out->paper.paper_len = curr->paper_info->paper.paper_len;
 	printf("It sould be copied: %ld", out->paper.paper_len);
 	return out;
 }
