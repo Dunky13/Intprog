1ce3ddda7674d9f067329ba435c03fcd39388e48
Marc Went <Dunky13@users.noreply.github.com>
2015-10-19 02:04:38 +0200

    Update paperserver.c

--- Assignment3/paperserver.c
+++ Assignment3/paperserver.c
@@ -4,22 +4,11 @@
 
 #include "rpcfunc.h"
 
-// struct papers
-// {
-// 	struct papers *next;
-// 	struct papers *prev;
-// 	int id;
-//
-// 	char* author;
-// 	char* title;
-// 	char* paper;
-// };
 struct paper_list_out* head;
 struct paper_list_out* tail;
-typedef enum { false, true } bool;
 //  [0,........,x]
 // [tail,.....,head]
-
+typedef enum { false, true } bool;
 
 void freePreviousListOut(struct paper_list_out* out)
 {
@@ -44,6 +33,29 @@ struct paper_list_out* closer(int id, struct paper_list_out* a, struct paper_lis
 	int toA = a == NULL ? INT_MAX : abs(a->id - id);
 	int toB = b == NULL ? INT_MAX : abs(b->id - id);
 	return toA < toB ? a : b;
+}
+
+struct paper_list_out* findPaper(int id){
+	struct paper_list_out* curr;
+	bool forward;
+	int i;
+
+	if(!hasPapers())
+	{
+		return NULL;
+	}
+	curr = closer(id, head, tail); //Not necessarily best option:
+	// [0,1,2,30,100] <= 30 is closer to 100 in this case. But worst O is O(n)
+	forward = curr == head ? false : true;
+
+	while(!isPaper(curr, id)){
+		curr = forward ? curr->next : curr->prev;
+		if(curr == NULL)
+		{
+			return NULL;
+		}
+	}
+	return curr;
 }
 
 bool hasPapers(){
@@ -77,29 +89,14 @@ int_out *remove_paper_1_svc(int_in *in, struct svc_req *req)
 	struct paper_list_out* curr;
 	struct paper_list_out* tmp;
 
-	bool forward;
+	curr = findPaper(id);
 
-	if(!hasPapers())
+	if(curr == NULL)
 	{
-		out = -1;
 		return &out;
 	}
 
-	curr = tail;
-	//curr = closer(id, head, tail); //Not necessarily best option:
-	// [0,1,2,30,100] <= 30 is closer to 100 in this case. But worst O is O(n)
-
-	forward = true;//curr == head ? false : true;
-
-	while(!isPaper(curr, id)){
-		curr = curr->next;//forward ? curr->next : curr->prev;
-		if(curr == NULL)
-		{
-			return &out;
-		}
-	}
-
-	if(curr == head && curr == tail)
+	else if(curr == head && curr == tail)
 	{
 		head 		= NULL;
 		tail 		= NULL;
@@ -147,24 +144,14 @@ paper_data *fetch_paper_1_svc(int_in *in, struct svc_req *req)
 		perror("Error allocating memory");
 		exit(1);
 	}
-	if(!hasPapers())
+
+	curr = findPaper(id);
+
+	if(curr == NULL)
 	{
 		out->paper_data_val = malloc(sizeof(char));
 		out->paper_data_len = 0;
 		return out;
-	}
-	curr = closer(id, head, tail); //Not necessarily best option:
-	// [0,1,2,30,100] <= 30 is closer to 100 in this case. But worst O is O(n)
-	forward = curr == head ? false : true;
-
-	while(!isPaper(curr, id)){
-		curr = forward ? curr->next : curr->prev;
-		if(curr == NULL)
-		{
-			out->paper_data_val = malloc(sizeof(char));
-			out->paper_data_len = 0;
-			return out;
-		}
 	}
 
 	out->paper_data_val = malloc(curr->paper_info->paper.paper_data_len * sizeof(char));
@@ -205,21 +192,11 @@ paper_information *info_paper_1_svc(int_in *in, struct svc_req *req)
 	out->title = "\0";
 	out->paper.paper_data_len =  0;
 	out->paper.paper_data_val =  malloc(sizeof(char));
-	if(!hasPapers())
+	curr = findPaper(id);
+
+	if(curr == NULL)
 	{
 		return out;
-	}
-	curr = closer(id, head, tail); //Not necessarily best option:
-	// [0,1,2,30,100] <= 30 is closer to 100 in this case. But worst O is O(n)
-	forward = curr == head ? false : true;
-
-	while(!isPaper(curr, id)){
-		printf("Looped %d", loop++);
-		curr = forward ? curr->next : curr->prev;
-		if(curr == NULL)
-		{
-			return out;
-		}
 	}
 
 	out->author = strdup(curr->paper_info->author);
@@ -257,7 +234,6 @@ paper_list_out *list_paper_1_svc(list_in *in, struct svc_req *req)
 	}
 
 	curr_out = out;
-	//curr_out->prev 				= (struct paper_list_out*) malloc(sizeof(struct paper_list_out));
 	while(curr != NULL){
 		curr_out->paper_info	= (struct paper_information*) malloc(sizeof(struct paper_information));
 		if(curr_out->paper_info == NULL)
