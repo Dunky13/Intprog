1001a7294da44de04b1e95bb4f9734806d3b23a4
Marc Went <Dunky13@users.noreply.github.com>
2015-10-22 21:04:19 +0200

    Delete config.h

--- Assignment4/paper/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ASSIGNMENT4__
-#define __ASSIGNMENT4__
-#define WEB_BASEPHP "/~ferry"
-#define WEB_BASECGI "/~mstamat/cgi-bin"
-#define HOTELGW_ADDRESS "localhost"
-#define HOTELGW_PORT 3333
-#define PAPER_ADDRESS "localhost"
-#define PAPER_PORT 3222
-#endif
