b24e7c19033a041f01cd40e4ff4d56cc1b905f46
Marc Went <Dunky13@users.noreply.github.com>
2015-10-23 16:04:50 +0200

    Update paperview.c

--- Assignment4/paper/paperview.c
+++ Assignment4/paper/paperview.c
@@ -7,7 +7,13 @@
 
 #define HOST "localhost"
 
+struct fileType{
+	char* contentType;
+	char* contentDisposition;
+};
+
 void printReason(char* reason){
+	printf("Content-type: application/json\r\n\r\n");
 	printf("{\"reason\": \"%s\"}", reason);
 }
 int parseInt(const char* argv)
@@ -35,31 +41,111 @@ CLIENT* createClient(){
 	}
 	return cl;
 }
-int getArticleInformation(CLIENT *cl, int article_id)
+
+int checkFile(char* file, int TEST[])
+{
+	int i;
+	int buffVal;
+	for(int i = 0; i < sizeof(TEST)/sizeof(TEST[0]); i++)
+	{
+		if(TEST[i] == 0x100)
+			continue;
+		buffVal = file[i] < 0 ? 256 + file[i]: file[i];
+		if(buffVal != TEST[i])
+		{
+			return 0;
+		}
+	}
+	return 1;
+	
+}
+
+struct fileType* getContentType(char* file){
+	int firstChar = file[0];
+	struct fileType *out = (struct fileType*) malloc(sizeof(struct fileType));
+	if(firstChar == 0x25)
+	{
+		if(checkFile(file, {0x25, 0x50, 0x44, 0x46})){
+			out->contentType = "application/pdf";
+			out->contentDisposition = ".pdf";
+			return out;
+		}
+	}
+	else if(firstChar == 0x50)
+	{
+		if(checkFile(file, {0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x06, 0x00})){
+			out->contentType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+			out->contentDisposition = ".docx";
+			return out;
+		}
+		if(checkFile(file, {0x50, 0x4B, 0x03, 0x04, 0x14, 0x00, 0x08})){
+			out->contentType = "application/java-archive";
+			out->contentDisposition = ".jar";
+			return out;
+		}
+		if(checkFile(file, {0x50, 0x4B, 0x03, 0x04, 0x0A, 0x00, 0x00})){
+			out->contentType = "application/zip";
+			out->contentDisposition = ".zip";
+			return out;
+		}
+	}
+	else if(firstChar == 0x0D || firstChar == 0xD0 || firstChar == 0xCF)
+	{
+		if(checkFile(file, {0xD0, 0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A, 0xE1}) ||
+			checkFile(file, {0xCF, 0x11, 0xE0, 0xA1, 0xB1, 0x1A, 0xE1, 0x00}) ||
+			checkFile(file, {0x0D, 0x44, 0x4F, 0x43})){
+			out->contentType = "application/msword";
+			out->contentDisposition = ".doc";
+			return out;
+		}
+	}
+	else if(firstChar == 0x7B)
+	{
+		if(checkFile(file, {0x7B, 0x5C, 0x72, 0x74, 0x66, 0x31})){
+			out->contentType = "text/richtext";
+			out->contentDisposition = ".rtf";
+			return out;
+		}
+	}
+	else if(firstChar == 0xFF)
+	{
+		if(checkFile(file, {0xFF, 0xD8, 0xFF})){
+			out->contentType = "image/jpeg";
+			out->contentDisposition = ".jpg";
+			return out;
+		}
+	}
+	else
+	{
+		out->contentType = "text/plain";
+		out->contentDisposition = "";
+		return out;
+	}
+}
+int getArticle(CLIENT *cl, int article_id)
 {
 	int_in in;
-	paper_information *out;
-
+	paper_data *out;
+	int i;
+	struct fileType* contentInfo;
 	in = (int_in) article_id;
 
-	out = info_paper_1(&in, cl);
+	out = fetch_paper_1(&in, cl);
 
 	if (out==NULL)
 	{
 		printReason("Get Article Information Error");
 		return 1;
 	}
-	if(!(strlen(out->author) == 0 && strlen(out->title) == 0))
+	contentInfo = getContentType(out->paper_data_val);
+	printf("Content-type: %s\r\n\r\n", contentInfo->contentType);
+	printf("Content-disposition: paper%02d%s\r\n\r\n", article_id, contentInfo->contentDisposition);
+	if(out->paper_data_len > 0)
 	{
-		printf("{");
-		printf("\t\"id:\" %d,\n", article_id);
-		printf("\t\"title:\" %s,\n", out->title);
-		printf("\t\"author:\" %s\n", out->author);
-		printf("}");
-	}
-	else
-	{
-		printReason("No Article Found");
+		for(i = 0; i < out->paper_data_len; i++)
+		{
+			printf("%c", out->paper_data_val[i]);
+		}
 	}
 	return 0;
 }
@@ -71,8 +157,6 @@ int main(int argc, char **argv) {
     int value;
 	const char *name = "id";
 	
-	fputs("Content-type: application/json\r\n\r\n", stdout);
-
     if ((varlist = CGI_get_all(0)) == NULL || 
 		varlist == 0 || 
 		(input = CGI_lookup(varlist, name)) == NULL)
@@ -90,7 +174,7 @@ int main(int argc, char **argv) {
 	}
 	value = 0;
 	cl = createClient();
-	value = getArticleInformation(cl, value);
+	value = getArticle(cl, value);
 	clnt_destroy(cl);
 	
 	return value;
