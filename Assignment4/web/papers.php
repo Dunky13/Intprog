67517209807b7777939bc48a2b7c18c70fc2f918
Marc Went <Dunky13@users.noreply.github.com>
2015-10-24 16:57:35 +0200

    Update papers.php

--- Assignment4/web/papers.php
+++ Assignment4/web/papers.php
@@ -3,15 +3,25 @@
 		<title>Papers.php</title>
 	</head>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script>
+	<script
 	$.ajax({
+		method: "GET",
 		url: "cgi-bin/papers.cgi",
 		success: function(data){
-			console.log(data);
+			$(".paperlist").empty();
+			$(data).each(function(index){
+				var item = $("<li>").attr("class", "paper");
+				var listID = $("<span>").attr("class", "id").text("["+this.id+"]");
+				var listAuthor = $("<span>").attr("class", "author").text(this.author);
+				var listTitle = $("<a>").attr("href", "cgi-bin/paperview.cgi?id="+this.id).attr("class", "title").text(this.title);
+				item.append(listID).append(listAuthor).append(listTitle);
+				$(".paperlist").append(item);
+			});
 		}
 	});
 	</script>
 	<body>
-	Hi
+		<ul class="paperlist">
+		</ul>
 	</body>
 </html>
