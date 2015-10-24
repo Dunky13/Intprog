fc14b73dada1ea906be00ed60ffdaa800420c432
Marc Went <Dunky13@users.noreply.github.com>
2015-10-24 16:28:37 +0200

    Update papers.php

--- Assignment4/web/papers.php
+++ Assignment4/web/papers.php
@@ -2,7 +2,7 @@
 	<head>
 		<title>Papers.php</title>
 	</head>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script>
 	$.ajax({
 		url: "cgi-bin/papers.cgi",
@@ -12,6 +12,6 @@
 	});
 	</script>
 	<body>
-	
+	Hi
 	</body>
 </html>
