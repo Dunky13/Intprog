4ed61cb416dc3e18bdb83a43fb133d79c7913435
Marc Went <Dunky13@users.noreply.github.com>
2015-10-25 12:54:40 +0100

    Update paperserror.php

--- Assignment4/web/paperserror.php
+++ Assignment4/web/paperserror.php
@@ -1,1 +1,12 @@
+<?php
+$errorMessage = isset($_GET['reason']) ? $_GET['reason'] : "Unknown reason";
+?>
 
+<html>
+	<head>
+		<title>Papererror.php</title>
+	</head>
+	<body>
+		<div class="new">An error has occurred: <span id="error"><?php echo $errorMessage;?></span></div>
+	</body>
+</html>
