747d85dac4f2c860fdb2e481241f083d4da95479
Marc Went <Dunky13@users.noreply.github.com>
2015-10-25 13:20:51 +0100

    Update paperload.php

--- Assignment4/web/paperload.php
+++ Assignment4/web/paperload.php
@@ -1,1 +1,40 @@
+<?php
+include("config.php");
+?>
 
+	<html>
+
+	<head>
+		<title>Papers.php</title>
+	</head>
+	<body>
+		<form enctype="multipart/form-data" method="post" action="<?php echo $WEB_BASECGI;?>/paperload.cgi">
+			<ul>
+
+				<li>
+					<label class="description" for="element_1">Titel </label>
+					<div>
+						<input id="element_1" name="element_1" class="element text medium" type="text" name="title" value="" />
+					</div>
+				</li>
+				<li id="li_2">
+					<label class="description" for="element_2">Author </label>
+					<div>
+						<input id="element_2" name="element_2" class="element text medium" type="text" name="author" value="" />
+					</div>
+				</li>
+				<li id="li_3">
+					<label class="description" for="element_3">Paper </label>
+					<div>
+						<input id="element_3" name="element_3" class="element file" name="file" type="file" />
+					</div>
+				</li>
+
+				<li class="buttons">
+					<input id="saveForm" type="submit" name="submit" value="Upload paper" />
+				</li>
+			</ul>
+		</form>
+	</body>
+
+	</html>
