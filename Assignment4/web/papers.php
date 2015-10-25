7a875972e328550ea11067a525b9750e4c1fcc19
Marc Went <Dunky13@users.noreply.github.com>
2015-10-25 21:30:10 +0100

    Update papers.php

--- Assignment4/web/papers.php
+++ Assignment4/web/papers.php
@@ -1,35 +1,18 @@
 <?php
-include("config.php");
-?>
 
-<html>
-	<head>
-		<title>Papers.php</title>
-	</head>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script>
-	$(document).ready(function(){
-	
-	$.ajax({
-		method: "GET",
-		url: "<?php echo $WEB_BASECGI;?>/papers.cgi",
-		success: function(data){
-			$(".paperlist").empty();
-			$(data).each(function(index){
-				var item = $("<li>").attr("class", "paper");
-				var listID = $("<span>").attr("class", "id").text("["+this.id+"]");
-				var listAuthor = $("<span>").attr("class", "author").text(this.author);
-				var listTitle = $("<a>").attr("href", "cgi-bin/paperview.cgi?id="+this.id).attr("class", "title").text(this.title);
-				item.append(listID).append(listAuthor).append(listTitle);
-				$(".paperlist").append(item);
-			});
-		}
-	});
-	
-	});
-	</script>
-	<body>
-		<ul class="paperlist"></ul>
-		<a href="<?php echo $WEB_BASEPHP; ?>">Go Back Home</a>
-	</body>
-</html>
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+
+$topdir=realpath(".");
+include('smarty/Smarty.class.php');
+$smarty = new Smarty;
+$smarty->setTemplateDir($topdir.'/smarty/templates');
+$smarty->setCompileDir($topdir.'/smarty/templates_c');
+$smarty->setCacheDir($topdir.'/smarty/cache');
+$smarty->setConfigDir($topdir.'/smarty/configs');
+
+require 'config.php';
+
+
+$smarty->display('tpl/papers.html');
