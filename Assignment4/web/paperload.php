da61261b27b9dad55a77844cb53e5a5fe2b4c74c
Marc Went <Dunky13@users.noreply.github.com>
2015-10-25 22:41:45 +0100

    Update paperload.php

--- Assignment4/web/paperload.php
+++ Assignment4/web/paperload.php
@@ -1,41 +1,18 @@
 <?php
-include("config.php");
-?>
 
-	<html>
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
-	<head>
-		<title>Paperload.php</title>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	</head>
-	<body>
-		<form enctype="multipart/form-data" method="post" action="<?php echo $WEB_BASECGI;?>/paperload.cgi">
-			<ul>
 
-				<li>
-					<label class="description" for="title">Titel </label>
-					<div>
-						<input id="title" class="element text medium" type="text" name="title" value="" />
-					</div>
-				</li>
-				<li id="li_2">
-					<label class="description" for="author">Author </label>
-					<div>
-						<input id="author" class="element text medium" type="text" name="author" value="" />
-					</div>
-				</li>
-				<li id="li_3">
-					<label class="description" for="file">Paper </label>
-					<div>
-						<input id="file" class="element file" name="file" type="file" />
-					</div>
-				</li>
+$topdir=realpath(".");
+include('smarty/Smarty.class.php');
+$smarty = new Smarty;
+$smarty->setTemplateDir($topdir.'/smarty/templates');
+$smarty->setCompileDir($topdir.'/smarty/templates_c');
+$smarty->setCacheDir($topdir.'/smarty/cache');
+$smarty->setConfigDir($topdir.'/smarty/configs');
 
-				<li class="buttons">
-					<input id="saveForm" type="submit" name="submit" value="Upload paper" />
-				</li>
-			</ul>
-		</form>
-	</body>
+require 'config.php';
+$smarty->assign('cgibase', $WEB_BASECGI);
 
-	</html>
+$smarty->display('tpl/paperload.html');
